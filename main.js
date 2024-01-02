3ca0cba54b5e2ce68795c9a426d341e1a5494e4a
Saulo Christian <saullochristian@outlook.com>
2024-01-02 11:30:25 -0300

    Criação do projeto

---
+++ main.js
@@ -0,0 +1,96 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+alert("Hello World!");
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+const num1 = 2;
+const num2 = 3;
+
+const soma = (num1 + num2);
+
+alert("A soma dos dois números é: " +soma);
+
+
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".    
+const num = 25;
+
+if (typeof num == 'number'){
+ alert("É um número")
+} else {
+    alert("Nãao é um número")
+}
+
+
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+const word = 'Saulo';
+
+if (typeof word == 'string'){
+ alert("É uma string")
+} else {
+    alert("Nãao é uma string")
+}
+
+
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+const boo = true;
+
+if (typeof boo == 'boolean'){
+ alert("É um booleano")
+} else {
+    alert("Nãao é um booleano")
+}
+
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+const num3 = 15;
+const num4 = 5;
+
+const sub = (num3 - num4);
+
+alert("A subtração dos dois números é: " +sub);
+
+
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+const num5 = 5;
+const num6 = 5;
+
+const mult = (num5 * num6);
+
+alert("A multiplicação dos dois números é: " +mult);
+
+
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+const num7 = 10;
+const num8 = 2;
+
+const div = (num7 / num8);
+
+alert("A subtração dos dois números é: " +div);
+
+
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba 
+// a mensagem "Não é um número par".
+const par = 8;
+
+if (par % 2 == 0){
+ alert("É um número par")
+} else {
+    alert("Nãao é um número par")
+}
+
+
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba
+// a mensagem "Não é um número ímpar".
+const impar = 5;
+
+if (impar % 2 != 0){
+ alert("É um número ímpar")
+} else {
+    alert("Nãao é um número ímpar")
+}
